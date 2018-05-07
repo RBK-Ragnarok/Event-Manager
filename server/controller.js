043cd938cfebce40a08afec86273662fa85ea46e
lenaSalamat <lenasalamat96@gmail.com>
2018-05-07 19:25:37 +0300

    Database functions

---
+++ server/controller.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var User=require('../database-mongo/user.js');
+var event=require('../database-mongo/Event.js');
+
+
+
+
+
+
+
+
+
