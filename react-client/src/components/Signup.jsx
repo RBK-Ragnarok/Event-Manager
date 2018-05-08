1ba1d74688eaa081e825eee39cb3734b8c9607ea
abdallahalramahi <abd_ram91@yahoo.com>
2018-05-08 10:27:35 +0300

    pkj json

---
+++ react-client/src/components/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Signup extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Signup
