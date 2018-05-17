56eb0fce824cd064311f0afbb1c596804ab981be
Belalfaouri <belalfaourig@gmail.com>
2018-05-17 12:44:35 +0300

    test

---
+++ tests/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {expect} from 'chai'
+import {mount,shallow} from 'enzyme'
+import Login from '../react-client/src/components/login.jsx'
+
+describe("<Home />",function(){
+  it('should render Home component')
+})
