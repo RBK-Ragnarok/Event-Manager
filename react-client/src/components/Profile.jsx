d68b2f19d3d0dc1b355c776f68050379092e296c
abdallahalramahi <abd_ram91@yahoo.com>
2018-05-22 14:39:45 +0300

    corrected some coding styles

--- react-client/src/components/Profile.jsx
+++ react-client/src/components/Profile.jsx
@@ -5,46 +5,46 @@ import {Route, Link, Switch, browserHistory, BrowserRouter as Router} from "reac
 import $ from 'jquery';
    
 class Profile extends React.Component {
-     constructor(props) {
-     super(props);
-     this.state = {
-     data:{},
-     sata:{},
-     username:'',
-     email:'',
-     age:'',
-     gender:'',
-     about:'',
-     editing:false
-     }
+    constructor(props) {
+	    super(props);
+	    this.state = {
+	    data:{},
+	    sata:{},
+	    username:'',
+	    email:'',
+	    age:'',
+	    gender:'',
+	    about:'',
+	    editing:false
+    	}
  
-    this.edit=this.edit.bind(this);
-    this.save=this.save.bind(this);
-    this.handelChange2=this.handelChange2.bind(this);
-    this.handelChange3=this.handelChange3.bind(this);
-    this.handelChange4=this.handelChange4.bind(this);
-    this.handelChange5=this.handelChange5.bind(this);
-    this.renderStart=this.renderStart.bind(this)
-    this.renderedit=this.renderedit.bind(this)
-   }
+	    this.edit=this.edit.bind(this);
+	    this.save=this.save.bind(this);
+	    this.handelChange2=this.handelChange2.bind(this);
+	    this.handelChange3=this.handelChange3.bind(this);
+	    this.handelChange4=this.handelChange4.bind(this);
+	    this.handelChange5=this.handelChange5.bind(this);
+	    this.renderStart=this.renderStart.bind(this)
+	    this.renderedit=this.renderedit.bind(this)
+    }
  
-   componentDidMount(){
-  	var that= this;
- 	$.ajax({
-	   url: '/user',
-	   type: 'GET',
-	   success: (data) => {
-        that.setState({data:data})
-	 }
-    })
-	$.ajax({
-		url: '/allevents',
-		type: 'GET',
-		success: (sata) => {
-		that.setState({sata:sata})       
-		} 
-	})
-  }	
+	componentDidMount(){
+  		var that= this;
+ 		$.ajax({
+	   		url: '/user',
+	   		type: 'GET',
+	   		success: (data) => {
+        		that.setState({data:data})
+	 		}
+    	})
+		$.ajax({
+			url: '/allevents',
+			type: 'GET',
+			success: (sata) => {
+				that.setState({sata:sata})       
+			} 
+		})
+  	}	
  
 	edit(){
 		this.setState({
@@ -62,30 +62,30 @@ class Profile extends React.Component {
 	      url: '/user',
 	      type: 'PUT',    
 	      data: this.state,
-		  success: function(result) {
+		  	success: function(result) {
 	          alert("Updated");
-	      }
+	      	}
 	    })
 	}
    
   	handelChange2(e){
     	this.setState({
-      	email:e.target.value
+      		email:e.target.value
     	})
   	}
   	handelChange3(e){
     	this.setState({
-      	age:e.target.value
+      		age:e.target.value
     	})
   	}
   	handelChange4(e){
     	this.setState({
-      	gender:e.target.value
+      		gender:e.target.value
     	})
   	}
   	handelChange5(e){
     	this.setState({
-      	about:e.target.value
+      		about:e.target.value
     	})
   	}
 
@@ -95,30 +95,32 @@ class Profile extends React.Component {
   			border: '5px solid black'
   		};	
  		return (
-		 	 <div className='container' style={divStyle}>
+		 	<div className='container' style={divStyle}>
  		 	 	<h1>Profile Page</h1>
  		 	 	<br />
  		 	 	<br />
-		 	 		<Row>
-		 	 		  <Col md={1}>
-		 	 		  </Col>
-		 	 		  <Col md={2}>
+		 	 	<Row>
+		 	 		<Col md={1}>
+		 	 		</Col>
+
+		 	 		<Col md={2}>
 		 	 		  	<span id="textcolor" >User Name: <p className="fa-1x"> {this.state.data.username}</p></span>	
- 		 	 		  </Col>
-		 	 		  <Col md={2}>
+ 		 	 		</Col>
+
+		 	 		<Col md={2}>
 		 	 		   	<span id="textcolor" >Email: <p className="fa-1x"> {this.state.data.email}</p></span>
-		 	 		  </Col>
+		 	 		</Col>
 
-		 	 		  <Col md={2}>
-		 	 		   	<span id="textcolor" >Age: <p className="fa-1x"> {this.state.data.age}</p></span>
- 		 	 		  </Col>
-		 	 		</Row>  
+		 	 		<Col md={2}>
+		 	 			<span id="textcolor" >Age: <p className="fa-1x"> {this.state.data.age}</p></span>
+ 		 	 		</Col>
+		 	 	</Row>  
 
-		 	 		  <Col md={2}>
-		 	 		  <Button bsStyle="success" onClick={this.edit}>Edit</Button> 
-		 	 		  </Col>	 		
-              </div>
- 		 	)
+		 	 	<Col md={2}>
+		 	 		<Button bsStyle="success" onClick={this.edit}>Edit</Button> 
+		 	 	</Col>	 		
+            </div>
+ 		)
 
   	}
 
@@ -130,44 +132,44 @@ class Profile extends React.Component {
 	    //      <img src={imgUrl}  width = '250px' className="img-thumbnail"/>
 	    //    </div>
 	    return(
-	      <div className="row">
-	      	<div className="col-md-3" style={{'paddingLeft':'20px'}}>
-	        </div>
+	    	<div className="row">
+	      		<div className="col-md-3" style={{'paddingLeft':'20px'}}>
+	        	</div>
 	        
-	        <div className='col-md-6'>
-	          <FormControl
-	            bsSize="large"
-	            value={this.state.email}
-	            placeholder="Email"
-	            onChange={this.handelChange2}
-	          />
-	          <hr></hr>
-	          <FormControl
-	            bsSize="large"
-	            value={this.state.age}
-	            placeholder="Age"
-	            onChange={this.handelChange3}
-	          />
-	          <hr></hr>
-	          <FormControl
-	            bsSize="large"
-	            value={this.state.gender}
-	            placeholder="Gender"
-	            onChange={this.handelChange4}
-	          />
-	          <hr></hr>
-	          <FormControl
-	            bsSize="large"
-	            value={this.state.about}
-	            placeholder="About"
-	            onChange={this.handelChange5}
-	          />
-	          <hr></hr>
-	          <Button bsStyle="success" onClick={this.save}>Save</Button>
-	        </div>
-	      </div>
+	        	<div className='col-md-6'>
+	       			<FormControl
+		            bsSize="large"
+		            value={this.state.email}
+		            placeholder="Email"
+		            onChange={this.handelChange2}
+	          		/>
+	          		<hr></hr>
+	         		<FormControl
+		            bsSize="large"
+		            value={this.state.age}
+		            placeholder="Age"
+		            onChange={this.handelChange3}
+		          	/>
+		          	<hr></hr>
+		          	<FormControl
+		            bsSize="large"
+		            value={this.state.gender}
+		            placeholder="Gender"
+		            onChange={this.handelChange4}
+		          	/>
+		          	<hr></hr>
+		          	<FormControl
+		            bsSize="large"
+		            value={this.state.about}
+		            placeholder="About"
+		            onChange={this.handelChange5}
+		         	/>
+	          		<hr></hr>
+	          		<Button bsStyle="success" onClick={this.save}>Save</Button>
+	        	</div>
+			</div>
 	    )
-   }
+    }
 
 	render() {
     	if(this.state.editing){
@@ -181,6 +183,6 @@ class Profile extends React.Component {
       		)
     	}
   	}
- }
+}
    
- export default Profile;
+export default Profile;
